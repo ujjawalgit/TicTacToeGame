a2103254eeb81257cbc6be50c7645ea29af64634
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-05-06 14:32:14 +0530

    added: Create a method to allow player to input X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,13 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static char[] board;
+    public static char playerLetter;
+    public static char computerLetter;
 
-    /*creating board  with empty spaces*/
+    /*creating board to play game with empty spaces*/
     private static char[] createBoard() {
         char[] board = new char[10];
         for (int i = 1; i < board.length; i++) {
@@ -11,10 +15,21 @@ public class TicTacToeGame {
         }
         return board;
     }
+    /* method to select letter of your choice*/
+    private static char chooseYourLetter() {
+        Scanner scanner = new Scanner(System.in);
+        char playerLetter = scanner.next().toUpperCase().charAt(0);
+        if( playerLetter!='X' && playerLetter!='O') {
+            System.out.print("Invalid Input Please enter O or X:");
+            playerLetter = scanner.next().toUpperCase().charAt(0);
+        }
+        return playerLetter;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToeGame");
-        /*createBoard method will create board with empty spaces*/
-        board = createBoard();
+        char[] board = createBoard(); /*createBoard method will create board with empty spaces*/
+        System.out.print("choose your letter(X or O):");
+        playerLetter = chooseYourLetter(); /* selecting letter for player*/
+        computerLetter = playerLetter == 'X' ? 'O' : 'X';/*if player has got X then computer will get O & vice-versa*/
     }
 }

d60887e0ca159f71129fbbfe9b8a73658276857d
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-05-07 10:35:03 +0530

    added : method showBoard to display the current Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -25,11 +25,24 @@ public class TicTacToeGame {
         }
         return playerLetter;
     }
+    /* method to show board*/
+    private static void showBoard() {
+        System.out.println( "      "+board[1]+"   |      "+board[2]+"     |  "+board[3]);
+        System.out.println("__________|____________|__________");
+        System.out.println( "      "+board[4]+"   |      "+board[5]+"     |  "+board[6]);
+        System.out.println("__________|____________|__________");
+        System.out.println( "      "+board[7]+"   |      "+board[8]+"     |  "+board[9]);
+        System.out.println("          |            |          ");
+    }
+    /**/
 
     public static void main(String[] args) {
-        char[] board = createBoard(); /*createBoard method will create board with empty spaces*/
+         board = createBoard(); /*createBoard method will create board with empty spaces*/
         System.out.print("choose your letter(X or O):");
         playerLetter = chooseYourLetter(); /* selecting letter for player*/
         computerLetter = playerLetter == 'X' ? 'O' : 'X';/*if player has got X then computer will get O & vice-versa*/
+        System.out.println("_______Game Board_______\n");
+        showBoard(); /* display board */
+
     }
 }
